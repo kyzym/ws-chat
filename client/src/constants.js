150dd59966617211ded5a04369aaefdfd49c67a3
kyzym <kyzym1@gmail.com>
2023-06-21 16:13:07 +0200

    feat: add env files 06/21/2023 16:13:07

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 export const VALID_ID_LENGTH = 24;
 export const API_URL = 'https://dummyjson.com/comments';
-export const SOCKET_URL = 'http://localhost:3000';
+export const SOCKET_URL = import.meta.env.VITE_APP_SOCKET_SERVER_URL;
