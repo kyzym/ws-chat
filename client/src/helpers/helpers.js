60d9b15a9763bb8fca8059270b54f2ff572fe3bb
kyzym <kyzym1@gmail.com>
2023-06-22 15:40:16 +0200

    refactor: remove client folder   06/22/2023 15:40:16

--- client/src/helpers/helpers.js
+++
@@ -1,5 +0,0 @@
-export const getInitials = (name) => {
-  const splitName = name.toUpperCase().split(' ');
-  if (splitName.length === 1) return splitName[0].substring(0, 2);
-  return splitName[0].substring(0, 1) + splitName[1].substring(0, 1);
-};
